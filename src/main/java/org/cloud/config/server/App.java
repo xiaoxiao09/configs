4184f14207bde2b92af03a4b91a8dea9ae2f48a9
xiaoxiao09 <676267900@qq.com>
2016-11-02 18:32:21 +0800

    test

---
+++ src/main/java/org/cloud/config/server/App.java
@@ -0,0 +1,19 @@
+package org.cloud.config.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new SpringApplicationBuilder(App.class).web(true).run(args);
+    }
+}
